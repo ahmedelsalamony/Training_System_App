7f2a01ad7acea7a159ba9ee145b0be2b7d64b3b1
ahmedabdelhameed79@gmail.com <salamony3>
2017-04-23 18:48:54 +0200

    third_commit

--- app/src/main/java/com/example/ahmed/training_system_app/launching/UserLoginFragment.java
+++ app/src/main/java/com/example/ahmed/training_system_app/launching/UserLoginFragment.java
@@ -1,11 +1,16 @@
 package com.example.ahmed.training_system_app.launching;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.ahmed.training_system_app.R;
 
@@ -14,21 +19,35 @@ import com.example.ahmed.training_system_app.R;
  */
 public class UserLoginFragment extends Fragment {
 
+   private EditText mUniqueNum,mPassword;
+   private TextView mContactUs;
+   private Button mLogin;
+   private View mView;
+   Typeface custom_font;
 
-    public UserLoginFragment() {
-        // Required empty public constructor
-    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        View v=  inflater.inflate(R.layout.lau_fragment_user_login, container, false);
+        mView =  inflater.inflate(R.layout.lau_fragment_user_login, container, false);
+        custom_font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/font.ttf");
+        mUniqueNum = (EditText) mView.findViewById(R.id.Edit_UserName);
+        mPassword = (EditText) mView.findViewById(R.id.Edit_Password);
+        mContactUs = (TextView) mView.findViewById(R.id.TextView_ContactUs);
+        mLogin = (Button) mView.findViewById(R.id.Button_Login);
+        mLogin.setTypeface(custom_font);
+        mUniqueNum.setTypeface(custom_font);
+        mPassword.setTypeface(custom_font);
+        mContactUs.setTypeface(custom_font);
 
+        return  mView;
+    }
 
+    public void userLogin(View view){
 
-        return  v;
     }
 
 }
